8b8b2c6d716aaf88baea170dcd77f266dbfeaec8
Eren Kaplan <ek.xcodeproject@gmail.com>
2022-10-11 03:16:56 +0300

    characters clickable and clicking on them increases the score

--- CatchTheKirb/ViewController.swift
+++ CatchTheKirb/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var score = 0
+    
 
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var highScoreLabel: UILabel!
@@ -28,9 +30,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scoreLabel.text = "Score \(score)"
+        
+        kirb1.isUserInteractionEnabled = true
+        kirb2.isUserInteractionEnabled = true
+        kirb3.isUserInteractionEnabled = true
+        kirb4.isUserInteractionEnabled = true
+        kirb5.isUserInteractionEnabled = true
+        kirb6.isUserInteractionEnabled = true
+        kirb7.isUserInteractionEnabled = true
+        kirb8.isUserInteractionEnabled = true
+        kirb9.isUserInteractionEnabled = true
+        
+        let regognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let regognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        
+        kirb1.addGestureRecognizer(regognizer1)
+        kirb2.addGestureRecognizer(regognizer2)
+        kirb3.addGestureRecognizer(regognizer3)
+        kirb4.addGestureRecognizer(regognizer4)
+        kirb5.addGestureRecognizer(regognizer5)
+        kirb6.addGestureRecognizer(regognizer6)
+        kirb7.addGestureRecognizer(regognizer7)
+        kirb8.addGestureRecognizer(regognizer8)
+        kirb9.addGestureRecognizer(regognizer9)
+        
+        
     }
-
+    
+    @objc func increaseScore(){
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+    }
 
 }
 
