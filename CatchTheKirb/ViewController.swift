1559fc1abe76d5f5eda70b1ed69e98c88bc1d5b3
Eren Kaplan <ek.xcodeproject@gmail.com>
2022-10-11 03:03:11 +0300

    storyboard definitions

--- CatchTheKirb/ViewController.swift
+++ CatchTheKirb/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    @IBOutlet weak var kirb1: UIImageView!
+    @IBOutlet weak var kirb2: UIImageView!
+    @IBOutlet weak var kirb3: UIImageView!
+    @IBOutlet weak var kirb4: UIImageView!
+    @IBOutlet weak var kirb5: UIImageView!
+    @IBOutlet weak var kirb6: UIImageView!
+    @IBOutlet weak var kirb7: UIImageView!
+    @IBOutlet weak var kirb8: UIImageView!
+    @IBOutlet weak var kirb9: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
